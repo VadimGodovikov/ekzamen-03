62f1d2fea1b6f51c22569e7ec6d8e7208795e963
VadimGodovikov <vadimka.332@mail.ru>
2024-04-19 09:00:43 +0300

    Описание класса Trip

--- ekzamen03/ekzamen03/Program.cs
+++ ekzamen03/ekzamen03/Program.cs
@@ -8,6 +8,13 @@ namespace ekzamen03
 {
     class Program
     {
+        class Trip
+        {
+            public string duration; // продолжительность
+            public int price; // цена
+            public int size; // размер группы
+        }
+
         static void Main(string[] args)
         {
         }
