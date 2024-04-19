d9d8a1b771b341e86cb463b82f547a8062cc64b2
VadimGodovikov <vadimka.332@mail.ru>
2024-04-19 10:29:14 +0300

    Описание метода сортировки

--- ekzamen03/ekzamen03/Program.cs
+++ ekzamen03/ekzamen03/Program.cs
@@ -14,12 +14,13 @@ namespace ekzamen03
             public string duration; // продолжительность
             public int price; // цена
             public int size; // размер группы
+
         }
 
         class TouristicOperator // туристический оператор
         {
             private int n = 0;
-            private Trip[] trips;
+            public Trip[] trips;
 
             public TouristicOperator(int n)
             {
@@ -27,10 +28,10 @@ namespace ekzamen03
                 trips = new Trip[n];
             }
 
-            /*public void SortTrip()
+            public void SortTrip() // сортировка по двум свойствам
             {
-                trips = trips.OrderBy
-            }*/
+                trips = trips.OrderBy(p => p.price).ThenByDescending(s => s.size).ToArray();
+            }
 
             public void WriteToFile(StreamWriter sw, int n)
             {
@@ -43,10 +44,42 @@ namespace ekzamen03
                 }
             }
 
+            public void Print()
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    Console.WriteLine("Продолжительность: " + trips[i].duration);
+                    Console.WriteLine("Цена: " + trips[i].price);
+                    Console.WriteLine("Размер группы: " + trips[i].size);
+                    Console.WriteLine();
+                }
+            }
+
         }
 
         static void Main(string[] args)
         {
+            Console.Write("Введите количество тур. поездок: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            TouristicOperator trips = new TouristicOperator(n);
+            for(int i = 0; i < n; i++)
+            {
+                trips.trips[i] = new Trip();
+                Console.Write("Введите продолжительность поездки: ");
+                trips.trips[i].duration = Console.ReadLine();
+                Console.Write("Введите цену поездки: ");
+                trips.trips[i].price = int.Parse(Console.ReadLine());
+                Console.Write("Введите размер группы: ");
+                trips.trips[i].size = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine();
+            trips.SortTrip();
+            
+
+
         }
     }
 }
